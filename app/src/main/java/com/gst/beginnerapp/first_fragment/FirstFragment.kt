2548f0ded480dbc497d371c56a42484bb1a2655a
Gideon <gideon@cicil.co.id>
2022-09-04 17:18:19 +0700

    add recyclerview

--- app/src/main/java/com/gst/beginnerapp/first_fragment/FirstFragment.kt
+++ app/src/main/java/com/gst/beginnerapp/first_fragment/FirstFragment.kt
@@ -1,7 +1,9 @@
 package com.gst.beginnerapp.first_fragment
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.gst.beginnerapp.databinding.FragmentFirstBinding
 
@@ -9,16 +11,41 @@ import com.gst.beginnerapp.databinding.FragmentFirstBinding
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
+    private var _binding: FragmentFirstBinding? = null
 
-    private var binding: FragmentFirstBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
+    private val mAdapter: FirstAdapter by lazy {
+        FirstAdapter()
+    }
+
+    private val dummyList = ArrayList<ModelData>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentFirstBinding.bind(view)
+        binding.apply {
+            with(rv){
+                dummyList.add(ModelData("one"))
+                dummyList.add(ModelData("two"))
+                dummyList.add(ModelData("three"))
+                dummyList.add(ModelData("four"))
+                adapter = mAdapter
+                mAdapter.updateData(dummyList)
+            }
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        binding = null
+        _binding = null
     }
 }
