2822c897a729dcc8f3ad1fb57fe1a7bce013aedd
Gideon <gideon@cicil.co.id>
2022-09-05 19:44:14 +0700

    add hilt in APP beginner  app

---
+++ app/src/main/java/com/gst/beginnerapp/BeginnerApp.kt
@@ -0,0 +1,14 @@
+package com.gst.beginnerapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+/**
+ * Created by gideon on 9/5/2022
+ * gideon@cicil.co.id
+ * https://www.cicil.co.id/
+ */
+
+@HiltAndroidApp
+class BeginnerApp : Application()
